f62e9a59b49ee1eb1f085ca855bf6d223669bf8e
Nattanon Nisup <sam_zheza@hotmail.com>
2024-04-16 15:42:00 -0400

    added commented section of function to the "GO" button; something like if the box is not filled, when click on the "Go" button, it will not go to the result page and give a warning meassge.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,13 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "resultwin.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    connect(ui->goButton, SIGNAL(clicked()), this, SLOT(on_goButton_clicked()));
+
 }
 
 MainWindow::~MainWindow()
@@ -19,6 +20,15 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_goButton_clicked()
 {
+    /*if(ui->song1->text().trimmed().isEmpty() ||
+        ui->song2->text().trimmed().isEmpty() ||
+        ui->song3->text().trimmed().isEmpty())
+    {
+        // If any box is empty, show a message box to the user
+        QMessageBox::warning(this,"Not Enough Songs", "Please Pick 3 Songs");
+        return;
+    }*/
+
     this->hide();
     auto resultWindow = new resultWin();
     resultWindow->setAttribute(Qt::WA_DeleteOnClose);
